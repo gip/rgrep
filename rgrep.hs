228e9324d7671fcf59e787611d82fa7242b89067
Gilles Pirio <g36130@gmail.com>
2013-02-08 11:01:55 -0800

    Total number of matches

--- rgrep.hs
+++ rgrep.hs
@@ -22,14 +22,14 @@ import Dirs
 textToBS :: T.Text -> B.ByteString
 textToBS = B.concat . BL.toChunks . T.encodeUtf8
 
-grep :: String -> Regex -> IO (Maybe Int)
+grep :: String -> Regex -> IO (Maybe (Int, Int))
 grep fn s = 
   do withFile fn ReadMode $ ((return $!) =<<) . liftM search . T.hGetContents
   where
     search text =
       let r = filter ((\x -> isJust (match s (textToBS x) []) ). snd) $ zip [1..] . T.lines $ text in
       case r of [] -> Nothing
-                (i,_):_ -> Just i
+                (i,_):_ -> Just (i, length r)
 
 rgrep :: String -> String -> [String] -> IO (Int , Int)
 rgrep pat dir kt = 
@@ -40,7 +40,7 @@ rgrep pat dir kt =
     f Dir (f,d) fn = return (f,d+1)
     f File (f,d) fn = 
       do g <- grep fn cpat
-         when (isJust g)  $ printf "Match at line %2d: %s\n" (fromJust g) fn
+         when (isJust g)  $ printf "Match at line %2d: %s  (%d lines matched in total)\n" (fst $ fromJust g) fn (snd $ fromJust g)
          return (if isJust g then f+1 else f,d)
     excl = (\n -> not $ ( foldr (\k a -> (isSuffixOf k n) || a) False kt ))
 
